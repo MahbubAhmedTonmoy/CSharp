245998c2b661fa802a7e5ba4223746f367636140
MahbubAhmedTonmoy <38882962+MahbubAhmedTonmoy@users.noreply.github.com>
2020-10-18 19:14:34 +0600

    partial class and method

---
+++ CSharp/CSharp/Partial/PartialClass1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace CSharp.Partial
+{
+
+    /// <summary>
+    /// partial class
+    /// </summary>
+    public partial class PurchaseService
+    {
+        public int GetTodayPurchase()
+        {
+            return 100;
+        }
+    }
+
+    public partial class PurchaseService
+    {
+        public int GetYesterdayPurchase()
+        {
+            return 200;
+        }
+    }
+    ///partial class end
+
+    /// <summary>
+    /// partial method
+    /// </summary>
+    public partial class UserService
+    {
+        // This file only contains 
+        // declaration of partial method 
+        partial void TrackUser(string userId);
+        public bool IsValidUser(string userId)
+        {
+            TrackUser(userId);
+            Guid userGuid;
+            bool ok = Guid.TryParse(userId, out userGuid);
+            return ok;
+        }
+    }
+    public partial class UserService
+    {
+        // This is the definition of 
+        // partial method 
+        partial void TrackUser(string userId)
+        {
+            Debug.WriteLine($"Tracked user id={userId}");
+        }
+    }
+
+
+}
---
+++ CSharp/CSharp/Partial/PartialClassTest.cs
@@ -0,0 +1,33 @@
+﻿using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace CSharp.Partial
+{
+    public class PartialClassTest
+    {
+        private readonly PurchaseService purchaseService = new PurchaseService();
+        private readonly UserService userService = new UserService();
+
+        [Fact]
+        public void ShouldReturnTodaysPurchaseAmount()
+        {
+            int result = purchaseService.GetTodayPurchase();
+            result.Should().Be(100);
+        }
+        [Fact]
+        public void ShouldReturnYesterdayPurchaseAmount()
+        {
+            int result = purchaseService.GetYesterdayPurchase();
+            result.Should().Be(200);
+        }
+        [Fact]
+        public void shouldUserIdValid()
+        {
+            var  result = userService.IsValidUser("34114393-176d-18ef-3fcf-203de94146f0");
+            result.Should().BeTrue();
+        }
+    }
+}
