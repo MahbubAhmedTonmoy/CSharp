4bfb8609a1f4293025a8fd2b024a7992059d63d7
MahbubAhmedTonmoy <38882962+MahbubAhmedTonmoy@users.noreply.github.com>
2020-10-20 09:03:20 +0600

    indexer

---
+++ CSharp/CSharp/Indexer/Indexer.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharp.Indexer
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+    public class Indexer
+    {
+        private readonly Student student = new Student { Id = 100, Name = "student" };
+
+        public string this[string index]
+        {
+            get
+            {
+                if(index == "Id") { return student.Id.ToString(); }
+                if(index == "Name") { return student.Name; }
+                throw new IndexOutOfRangeException();
+            }
+            set
+            {
+                if (index == "Id") { student.Id = Convert.ToInt32(value); }
+                if (index == "Name") { student.Name = value; }
+                throw new IndexOutOfRangeException();
+            }
+        }
+
+        //list
+        public readonly List<Student> studentSet = new List<Student> { new Student { Id = 101, Name = "student1" },
+         new Student { Id = 102, Name = "student2" }, new Student { Id = 103, Name = "student3" },
+            new Student { Id = 104, Name = "student4" },};
+
+        public Student this[int index]
+        {
+            get
+            {
+                Student student = studentSet[index];
+                return student;
+            }
+            set
+            {
+                studentSet[index] = value;
+                throw new IndexOutOfRangeException();
+            }
+        }
+    }
+}
---
+++ CSharp/CSharp/Indexer/IndexerTest.cs
@@ -0,0 +1,38 @@
+﻿using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace CSharp.Indexer
+{
+    public class IndexerTest
+    {
+        private readonly Indexer indexer = new Indexer();
+
+        [Fact]
+        public void ShouldGetItemFromObjectWhenValidIndexProvided()
+        {
+            string id = indexer["Id"];
+            string name = indexer["Name"];
+            id.Should().Be("100");
+            name.Should().Be("student");
+        }
+
+        [Fact]
+        public void ShouldGetObjectStudentSet()
+        {
+            Student student = indexer[0];
+            student.Id.Should().Be(101);
+        }
+        [Fact]
+        public void ShouldAddObjectStudentSet()
+        {
+
+            Student student = new Student { Id = 105, Name = "student5" };
+            indexer[4] = student;
+            Student student5 = indexer[4];
+            student5.Id.Should().Be(105);
+        }
+    }
+}
