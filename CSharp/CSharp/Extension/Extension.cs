406446af997f698b4ca01a3f1b836a8ae505b58c
MahbubAhmedTonmoy <38882962+MahbubAhmedTonmoy@users.noreply.github.com>
2020-12-20 18:18:26 +0600

    Extension optional

---
+++ CSharp/CSharp/Extension/Extension.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace CSharp.Extension
+{
+    class Extension
+    {
+    }
+    public class Calculator
+    {
+        public int Add(int number1, int number2)
+        {
+            return number1 + number2;
+        }
+    }
+
+    public static class CalculatorExtension
+    {
+        public static int AddList(this Calculator calculator, List<int> numbers)
+        {
+            int ans = 0;
+            foreach(var i in numbers)
+            {
+                ans += i;
+            }
+            return ans;
+        }
+        public static int AddmultipleItems(this Calculator calculator,int number1,[Optional] List<int> numbers)
+        {
+            int ans = 0;
+            if (number1 != null) { ans += number1; }
+            if(numbers != null)
+            {
+                foreach (var i in numbers)
+                {
+                    ans += i;
+                }
+            }
+            return ans;
+        }
+    }
+}
---
+++ CSharp/CSharp/Extension/ExtensionTest.cs
@@ -0,0 +1,35 @@
+﻿using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace CSharp.Extension
+{
+    public class ExtensionTest
+    {
+        [Fact]
+        public void ShouldSum()
+        {
+            Calculator c1 = new Calculator();
+            int ans = c1.Add(1, 2);
+            ans.Should().Be(3);
+        }
+        [Fact]
+        public void ShouldExtensionSumWork()
+        {
+            Calculator c1 = new Calculator();
+            int ans = c1.AddList(new List<int> { 1,2,3});
+            ans.Should().Be(6);
+        }
+        [Fact]
+        public void ShouldAddmultipleItemsSumWork()
+        {
+            Calculator c1 = new Calculator();
+            int ans = c1.AddmultipleItems(2,new List<int> { 1, 2, 3 });
+            int ans2 = c1.AddmultipleItems(1);
+            ans.Should().Be(8);
+            ans2.Should().Be(1);
+        }
+    }
+}
